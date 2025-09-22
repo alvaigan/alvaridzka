41370101694844c827dbd0dac2129441ffe10c24
Alvaigan <aalvaigan@gmail.com>
2025-09-22 21:59:15 +0700

    fix: update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import {Aref_Ruqaa, Chivo, Cookie, Playfair_Display } from "next/font/google";
+import { Aref_Ruqaa, Chivo, Cookie, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { AudioProvider } from "@/context/AudioContext";
-
 
 const chivo = Chivo({
   subsets: ["latin"],
@@ -15,21 +14,21 @@ const cookie = Cookie({
   variable: "--font-cookie",
   style: ["normal"],
   weight: ["400"],
-}) 
+});
 
 const arefRuqa = Aref_Ruqaa({
   subsets: ["latin"],
   variable: "--font-aref-ruqa",
   style: ["normal"],
   weight: ["400", "700"],
-})
+});
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-playfair",
   style: ["normal", "italic"],
   weight: ["400", "500", "600", "700", "800", "900"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Alvaigan & Ridzka Nur Fajrie",
@@ -39,26 +38,21 @@ export const metadata: Metadata = {
     description: "The Wedding of Alvaigan & Ridzka Nur Fajrie",
     images: [
       {
-        url: "https://alvaridzka.site/couple.jpg", // Replace with your actual domain
-        width: 1200,
-        height: 630,
-        alt: "Alvaigan & Ridzka Nur Fajrie",
-      },
-      {
-        url: "https://alvaridzka.site/main-couple.png", // Alternative image
-        width: 1080,
-        height: 1080,
+        url: "https://alvaridzka.site/couple.jpg",
+        width: 300,
+        height: 300,
         alt: "Alvaigan & Ridzka Nur Fajrie",
       },
     ],
     type: "website",
     locale: "en_US",
+    url: "https://alvaridzka.site",
   },
   twitter: {
     card: "summary_large_image",
     title: "Alvaigan & Ridzka Nur Fajrie",
     description: "The Wedding of Alvaigan & Ridzka Nur Fajrie",
-    images: ["https://alvaridzka.site/couple.jpg"], // Replace with your actual domain
+    images: ["https://alvaridzka.site/couple.jpg"],
   },
 };
 
@@ -72,9 +66,7 @@ export default function RootLayout({
       <body
         className={` ${chivo.variable} ${cookie.variable} ${arefRuqa.variable} ${playfair.variable} antialiased`}
       >
-        <AudioProvider>
-          {children}
-        </AudioProvider>
+        <AudioProvider>{children}</AudioProvider>
       </body>
     </html>
   );
